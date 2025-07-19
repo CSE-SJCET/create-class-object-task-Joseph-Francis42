6db0a5c34f4ee7af910946a32235b49af5d753eb
Joseph-Francis42 <josephfrancis2028@cs.sjcetpalai.ac.in>
2025-07-19 20:21:03 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ class Student{
           System.out.println("Roll Number: "+rollNumber);
           System.out.println("Grade: "+grade);
      }    // TODO: Create a class called Student with name, rollNumber, and grade
-
+}
     // TODO: Create a method to display student details
 public class Main {
       public static void main(String[] args) {
